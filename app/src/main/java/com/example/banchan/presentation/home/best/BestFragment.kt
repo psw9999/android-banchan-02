442ce632200be391a54201e9da25e22fd856906f
woowahan <nosod3@naver.com>
2022-08-10 01:22:45 +0900

    [FEAT] #11 - RecyclerView Item간 간격 적용

--- app/src/main/java/com/example/banchan/presentation/home/best/BestFragment.kt
+++ app/src/main/java/com/example/banchan/presentation/home/best/BestFragment.kt
@@ -1,60 +1,43 @@
 package com.example.banchan.presentation.home.best
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.graphics.Rect
 import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.banchan.R
+import com.example.banchan.databinding.FragmentBestBinding
+import com.example.banchan.fakeBestItem
+import com.example.banchan.presentation.base.BaseFragment
+import com.example.banchan.presentation.home.best.adapter.BestListAdapter
+import com.example.banchan.util.dimen.dpToPx
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class BestFragment : BaseFragment<FragmentBestBinding>(R.layout.fragment_best) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BestFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BestFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val bestListAdapter by lazy { BestListAdapter() }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun initViews() {
+        initRecyclerView()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_best, container, false)
+    override fun observe() {
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BestFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BestFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initRecyclerView(){
+        binding.rvBestList.adapter = bestListAdapter
+        binding.rvBestList.addItemDecoration(object: RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                super.getItemOffsets(outRect, view, parent, state)
+                val position = parent.getChildAdapterPosition(view)
+                if (position == 0) outRect.bottom = dpToPx(requireContext(), 24)
+                else outRect.bottom = dpToPx(requireContext(), 40)
             }
+        })
+        bestListAdapter.submitList(fakeBestItem)
     }
+
 }
