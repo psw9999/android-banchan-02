0be2adc71e79bf997595b858b7d4c71780ab8993
woowahan <nosod3@naver.com>
2022-08-10 21:56:29 +0900

    [CHORE] #14 - print문 삭제

--- app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
+++ app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
@@ -6,7 +6,6 @@ import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageFromUrl(url: String) {
-    println("야호야호호"+url)
     Glide.with(this.context)
         .load(url)
         .into(this)
