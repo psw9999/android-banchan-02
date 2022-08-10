e934a77408b25717f996aca7365c987c9dea8211
woowahan <nosod3@naver.com>
2022-08-10 21:50:17 +0900

    [FEAT] #14 - 홈화면의 Empty, Error, Header, Loading ViewHolder 추가

---
+++ app/src/main/java/com/example/banchan/presentation/adapter/home/HomeErrorViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.banchan.presentation.adapter.home
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.banchan.databinding.ItemHomeErrorBinding
+
+class HomeErrorViewHolder(binding: ItemHomeErrorBinding) : RecyclerView.ViewHolder(binding.root)
