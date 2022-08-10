e8b4044474ed6b64c9a3937253738554348b0537
woowahan <nosod3@naver.com>
2022-08-10 14:57:31 +0900

    [FEAT] #13 - Body, Item toModel 메서드 추가 및 Item Id 삭제 (Hash 값으로 구분)

--- app/src/main/java/com/example/banchan/data/response/best/Body.kt
+++ app/src/main/java/com/example/banchan/data/response/best/Body.kt
@@ -1,5 +1,6 @@
 package com.example.banchan.data.response.best
 
+import com.example.banchan.domain.model.BestModel
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -9,4 +10,13 @@ data class Body(
     val categoryId: String,
     val items: List<Item>,
     val name: String
-)
+) {
+    fun toBestModel() : BestModel {
+        return BestModel(
+            title = name,
+            items = items.map{
+                it.toItemModel()
+            }
+        )
+    }
+}
