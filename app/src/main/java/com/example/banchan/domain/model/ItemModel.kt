7ef5716cd3e04b9749be37b7e602d368e2d0db54
woowahan <nosod3@naver.com>
2022-08-14 01:57:06 +0900

    [FIX] #17 - ItemModel isCartAdded val -> var type으로 수정

--- app/src/main/java/com/example/banchan/domain/model/ItemModel.kt
+++ app/src/main/java/com/example/banchan/domain/model/ItemModel.kt
@@ -1,9 +1,5 @@
 package com.example.banchan.domain.model
 
-import androidx.annotation.StringRes
-import com.example.banchan.R
-import com.example.banchan.presentation.adapter.main.Type
-
 data class ItemModel(
     val description: String,
     val detailHash: String,
@@ -12,5 +8,5 @@ data class ItemModel(
     val discountPrice: String? = null,
     val originPrice: String,
     val title: String,
-    val isCartAdded: Boolean = false
+    var isCartAdded: Boolean = false
 )
