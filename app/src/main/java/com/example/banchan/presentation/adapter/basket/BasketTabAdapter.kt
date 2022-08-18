7c71ec0a51a78272841cb6052227534dec788bef
woowahan <nosod3@naver.com>
2022-08-19 00:18:09 +0900

    [FEAT] #59 - 장바구니 탭 전체 선택, 전체 선택 해제, 선택 삭제 클릭 리스너 주입

--- app/src/main/java/com/example/banchan/presentation/adapter/basket/BasketTabAdapter.kt
+++ app/src/main/java/com/example/banchan/presentation/adapter/basket/BasketTabAdapter.kt
@@ -4,14 +4,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class BasketTabAdapter(
-
+    private val onClickCheckBoxTab: ((Int)->Unit),
+    private val onClickSeletedDeleteBtn: (()->Unit)
 ) : RecyclerView.Adapter<BasketTabViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasketTabViewHolder
         = BasketTabViewHolder.create(parent)
 
     override fun onBindViewHolder(holder: BasketTabViewHolder, position: Int) {
-
+        holder.bind(onClickCheckBoxTab, onClickSeletedDeleteBtn)
     }
 
     override fun getItemCount(): Int = 1
