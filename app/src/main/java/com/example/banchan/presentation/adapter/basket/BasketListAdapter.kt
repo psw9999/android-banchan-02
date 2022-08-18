86a08f9380e49a92f7ec79d7b5687985ca331155
woowahan <nosod3@naver.com>
2022-08-19 00:19:08 +0900

    [FEAT] #59 - 장바구니 리스트 어댑터 체크박스, 삭제버튼 클릭 리스너 주입 및 페이로드 적용

--- app/src/main/java/com/example/banchan/presentation/adapter/basket/BasketListAdapter.kt
+++ app/src/main/java/com/example/banchan/presentation/adapter/basket/BasketListAdapter.kt
@@ -6,7 +6,8 @@ import androidx.recyclerview.widget.ListAdapter
 import com.example.banchan.domain.model.BasketModel
 
 class BasketListAdapter(
-
+    private val onCheckBoxClick: ((BasketModel) -> Unit),
+    private val onClickDeleteBtn: ((BasketModel) -> Unit)
 ) : ListAdapter<BasketModel, BasketItemViewHolder>(diffUtil) {
 
     companion object {
@@ -18,13 +19,34 @@ class BasketListAdapter(
             override fun areContentsTheSame(oldItem: BasketModel, newItem: BasketModel): Boolean {
                 return oldItem == newItem
             }
+
+            override fun getChangePayload(oldItem: BasketModel, newItem: BasketModel): Any? {
+                return (oldItem.count != newItem.count) || (oldItem.isChecked != newItem.isChecked)
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasketItemViewHolder
-        = BasketItemViewHolder.create(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasketItemViewHolder =
+        BasketItemViewHolder.create(parent)
 
     override fun onBindViewHolder(holder: BasketItemViewHolder, position: Int) {
-        holder.binding(getItem(position))
+        holder.initBind(getItem(position), onCheckBoxClick, onClickDeleteBtn)
+    }
+
+    override fun onBindViewHolder(
+        holder: BasketItemViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()) {
+            super.onBindViewHolder(holder, position, payloads)
+        } else {
+            if (payloads[0] as Boolean) holder.refreshBind(
+                getItem(position),
+                onCheckBoxClick,
+                onClickDeleteBtn
+            )
+            else super.onBindViewHolder(holder, position, payloads)
+        }
     }
 }
