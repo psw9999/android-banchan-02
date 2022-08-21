cc8bfbc3e95bdb4f1f974a08dee00e1732ac558f
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-08-21 11:03:46 +0900

    [FEAT] #78 배송 완료 notification 구현

--- app/src/main/java/com/example/banchan/data/source/local/history/History.kt
+++ app/src/main/java/com/example/banchan/data/source/local/history/History.kt
@@ -2,6 +2,7 @@ package com.example.banchan.data.source.local.history
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.banchan.util.DEFAULT_DELIVERY_TIME
 import java.util.*
 
 @Entity
@@ -9,5 +10,5 @@ data class History(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val date: Date = Date(),
-    val remainTime: Int = 5
+    val remainTime: Int = DEFAULT_DELIVERY_TIME
 )
