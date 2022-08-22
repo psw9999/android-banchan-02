7968dfb03e739ba8c165d0a4c1bc281e93fd8345
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-08-22 23:07:11 +0900

    [REFACTOR] #94 UiStat로 관리, 네트워크 error시 refresh 기능 추가

--- app/src/main/java/com/example/banchan/presentation/UiState.kt
+++ app/src/main/java/com/example/banchan/presentation/UiState.kt
@@ -1,9 +1,9 @@
 package com.example.banchan.presentation
 
 sealed class UiState<out T> {
-    object Init: UiState<Nothing>()
-    object Loading: UiState<Nothing>()
-    class Success<out T>(val item: T): UiState<T>()
+    object Init : UiState<Nothing>()
+    object Loading : UiState<Nothing>()
+    class Success<out T>(val item: T) : UiState<T>()
     class Error(val throwable: Throwable?) : UiState<Nothing>()
-    object Empty: UiState<Nothing>()
+    object Empty : UiState<Nothing>()
 }
