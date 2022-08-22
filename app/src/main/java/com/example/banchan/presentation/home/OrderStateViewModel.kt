137e31a9fcf0ff7dab0c8475f7902c95826c9ea8
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-08-22 11:22:35 +0900

    [REFACTOR] #97 OrderListViewModel -> OrderStateViewModel로 수정

--- app/src/main/java/com/example/banchan/presentation/home/OrderStateViewModel.kt
+++ app/src/main/java/com/example/banchan/presentation/home/OrderStateViewModel.kt
@@ -3,6 +3,8 @@ package com.example.banchan.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.banchan.data.source.local.history.History
+import com.example.banchan.domain.model.OrderListModel
+import com.example.banchan.domain.usecase.history.GetHistoryByIdUseCase
 import com.example.banchan.domain.usecase.history.GetHistoryListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -10,7 +12,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OrderStateViewModel @Inject constructor(
-    getHistoryListUseCase: GetHistoryListUseCase
+    getHistoryListUseCase: GetHistoryListUseCase,
+    private val getHistoryByIdUseCase: GetHistoryByIdUseCase
 ) : ViewModel() {
 
     private val historyListFlow: Flow<List<History>> =
@@ -26,4 +29,26 @@ class OrderStateViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = false
         )
+
+    val historyModelFlow: Flow<List<OrderListModel>> =
+        historyListFlow.map { historyList ->
+            val historyModelList = mutableListOf<OrderListModel>()
+            historyList.forEach { history ->
+                getHistoryByIdUseCase(history.id).first().onSuccess { historyWithItems ->
+                    if (historyWithItems.items.isNotEmpty()) {
+                        historyModelList.add(
+                            OrderListModel(
+                                id = history.id,
+                                thumbNailImage = historyWithItems.items[0].imageUrl,
+                                name = historyWithItems.items[0].name,
+                                numberOfProduct = historyWithItems.items.size,
+                                price = history.deliveryFee + historyWithItems.items.sumOf { it.originPrice * it.count },
+                                isCompleted = history.isSuccess
+                            )
+                        )
+                    }
+                }
+            }
+            historyModelList
+        }
 }
