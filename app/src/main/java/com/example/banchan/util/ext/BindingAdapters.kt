c879fbd192a7120d48f9f63b2ad90eaa598ceed4
woowahan <nosod3@naver.com>
2022-08-22 21:30:14 +0900

    [REFACTOR] #34 - Glide placeholder, error 이미지 추가, sectionImage 부분 수정

--- app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
+++ app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
@@ -3,11 +3,15 @@ package com.example.banchan.util.ext
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
+import com.example.banchan.R
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageFromUrl(url: String) {
     Glide.with(this.context)
         .load(url)
+        .placeholder(R.drawable.ic_baseline_restaurant_menu_24)
+        .error(R.drawable.ic_baseline_error_24)
         .into(this)
 }
 
@@ -15,6 +19,9 @@ fun ImageView.setImageFromUrl(url: String) {
 fun ImageView.setSectionImage(url: String) {
     Glide.with(this.context)
         .load(url)
-        .override(1000,1000)
+        .placeholder(R.drawable.ic_baseline_restaurant_menu_24)
+        .skipMemoryCache(true)
+        .error(R.drawable.ic_baseline_error_24)
+        .override(SIZE_ORIGINAL, SIZE_ORIGINAL)
         .into(this)
 }
