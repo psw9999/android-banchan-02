0d7b61e62cfeecf0de60b75acf66806e7746e040
woowahan <nosod3@naver.com>
2022-08-24 17:20:26 +0900

    [FEAT] #115 - UiState의 DataBinding Extension 함수 추가

--- app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
+++ app/src/main/java/com/example/banchan/util/ext/BindingAdapters.kt
@@ -1,10 +1,12 @@
 package com.example.banchan.util.ext
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.example.banchan.R
+import com.example.banchan.presentation.UiState
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageFromUrl(url: String) {
@@ -24,4 +26,24 @@ fun ImageView.setSectionImage(url: String) {
         .error(R.drawable.ic_baseline_error_24)
         .override(SIZE_ORIGINAL, SIZE_ORIGINAL)
         .into(this)
+}
+
+@BindingAdapter("showOnSuccess")
+fun showOnSuccess(view: View, uiState: UiState<*>) {
+    view.visibility = if (uiState is UiState.Success) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("showOnLoading")
+fun showOnLoading(view: View, uiState: UiState<*>) {
+    view.visibility = if (uiState is UiState.Loading) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("showOnError")
+fun showOnError(view: View, uiState: UiState<*>) {
+    view.visibility = if (uiState is UiState.Error) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("showOnEmpty")
+fun showOnEmpty(view: View, uiState: UiState<*>) {
+    view.visibility = if (uiState is UiState.Empty) View.VISIBLE else View.INVISIBLE
 }
