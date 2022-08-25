7af4b0521843d75bda5526cf23127f8ed93c5108
woowahan <nosod3@naver.com>
2022-08-25 16:22:35 +0900

    [REFACTOR] #118 - BasketListViewModel BasketList UiState 적용

--- app/src/main/java/com/example/banchan/presentation/basket/BasketListViewModel.kt
+++ app/src/main/java/com/example/banchan/presentation/basket/BasketListViewModel.kt
@@ -6,16 +6,16 @@ import com.example.banchan.data.response.detail.DetailResponse
 import com.example.banchan.data.source.local.basket.BasketItem
 import com.example.banchan.data.source.local.history.HistoryItem
 import com.example.banchan.domain.model.BasketModel
-import com.example.banchan.domain.model.ItemModel
 import com.example.banchan.domain.model.OrderModel
 import com.example.banchan.domain.usecase.basket.*
 import com.example.banchan.domain.usecase.detail.GetProductDetailUseCase
 import com.example.banchan.domain.usecase.history.InsertHistoryItemsUseCase
-import com.example.banchan.domain.usecase.recently.GetRecentProductUseCase
+import com.example.banchan.presentation.UiState
 import com.example.banchan.util.ext.toNum
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,67 +29,81 @@ class BasketListViewModel @Inject constructor(
     private val insertHistoryItemsUseCase: InsertHistoryItemsUseCase
 ) : ViewModel() {
 
-    private val basketDbFlow: Flow<List<BasketItem>> =
+    private val basketList: Flow<List<BasketItem>?> =
         getBasketItemUseCase().map { result ->
-            result.onSuccess { return@map it }
-            listOf()
+            if (result.isSuccess) return@map result.getOrNull()
+            else return@map null
         }
 
-    private val _isBasketLoading = MutableStateFlow(true)
-    private val _isRecentlyLoading = MutableStateFlow(true)
-
-    fun setIsBasketLoading(isLoading: Boolean) {
-        _isBasketLoading.value = isLoading
-    }
-
-    fun setIsRecentlyLoading(isLoading: Boolean) {
-        _isRecentlyLoading.value = isLoading
-    }
-
-    val isLoading =
-        combine(_isBasketLoading, _isRecentlyLoading) { isBasketLoading, isRecentlyLoading ->
-            isBasketLoading || isRecentlyLoading
-        }
+    private val basketRefresh = MutableSharedFlow<Boolean>(replay = 1)
 
     val successHistoryId = MutableSharedFlow<Long>()
-
     private val detailApiMap: MutableMap<String, DetailResponse> = mutableMapOf()
 
-    val basketItemFlow: Flow<List<BasketModel>> =
-        basketDbFlow
-            .map { basketList ->
-                return@map basketList.mapNotNull { basketItem ->
-                    checkDetailApiMap(basketItem.hash)
-                    return@mapNotNull detailApiMap[basketItem.hash]?.toBasketModel(
-                        name = basketItem.name,
-                        count = basketItem.count,
-                        isSelected = basketItem.isSelected
-                    )
-                }
-            }
+    private val _basketUiState: MutableStateFlow<UiState<List<BasketModel>>> = MutableStateFlow(UiState.Init)
+    val basketUiState = _basketUiState.asStateFlow()
 
-    val isAllBasketItemSelectedFlow: Flow<Boolean> =
-        basketDbFlow
-            .map { basketList ->
-                return@map basketList.all { it.isSelected }
-            }
-
-    val basketAmountSumFlow: Flow<OrderModel> =
-        basketDbFlow
-            .map { basketList ->
-                var amount = 0
-                basketList.forEach { basketItem ->
-                    if (basketItem.isSelected) {
-                        checkDetailApiMap(basketItem.hash)
-                        detailApiMap[basketItem.hash]?.let { detailResponse ->
-                            val priceList = detailResponse.data.prices
-                            amount += if (priceList.size == 1) (priceList[0].toNum() * basketItem.count)
-                            else (priceList[1].toNum() * basketItem.count)
+    init {
+        viewModelScope.launch {
+            basketRefresh.emit(true)
+            combine(basketList, basketRefresh) { basketList, _ ->
+                basketList
+            }.collect { basketList ->
+                _basketUiState.update {
+                    if(basketList == null) return@update UiState.Error(Exception("DB Error"))
+                    else {
+                        if (basketList.isEmpty()) return@update UiState.Empty
+                        try {
+                            val result = basketList.mapNotNull { basketItem ->
+                                checkDetailApiMap(basketItem.hash)
+                                return@mapNotNull detailApiMap[basketItem.hash]?.toBasketModel(
+                                    name = basketItem.name,
+                                    count = basketItem.count,
+                                    isSelected = basketItem.isSelected
+                                )
+                            }
+                            return@update UiState.Success(result)
+                        } catch (e: Exception) {
+                            return@update UiState.Error(Exception("Network Error"))
                         }
                     }
                 }
-                if (amount >= 40000) OrderModel(orderPrice = amount, deliveryFee = 0)
-                else OrderModel(orderPrice = amount, deliveryFee = 2500)
+            }
+        }
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            basketRefresh.emit(true)
+        }
+    }
+
+    val isAllBasketItemSelectedFlow: Flow<Boolean> =
+        basketList
+            .map { basketList ->
+                return@map basketList?.all { it.isSelected } ?: false
+            }
+
+    val basketAmountSumFlow: Flow<OrderModel> =
+        basketList
+            .map { basketList ->
+                var amount = 0
+                try {
+                    basketList?.forEach { basketItem ->
+                        if (basketItem.isSelected) {
+                            checkDetailApiMap(basketItem.hash)
+                            detailApiMap[basketItem.hash]?.let { detailResponse ->
+                                val priceList = detailResponse.data.prices
+                                amount += if (priceList.size == 1) (priceList[0].toNum() * basketItem.count)
+                                else (priceList[1].toNum() * basketItem.count)
+                            }
+                        }
+                    }
+                    if (amount >= 40000) OrderModel(orderPrice = amount, deliveryFee = 0)
+                    else OrderModel(orderPrice = amount, deliveryFee = 2500)
+                } catch(e : Exception) {
+                    OrderModel(orderPrice = 0, deliveryFee = 2500)
+                }
             }
 
     private suspend fun checkDetailApiMap(hash: String) {
@@ -97,6 +111,9 @@ class BasketListViewModel @Inject constructor(
             getProductDetailUseCase.invoke(hash)
                 .onSuccess {
                     detailApiMap[hash] = it
+                }
+                .onFailure {
+                    throw it
                 }
         }
     }
@@ -133,7 +150,7 @@ class BasketListViewModel @Inject constructor(
                     hash = basketModel.detailHash,
                     name = basketModel.name,
                     count = basketModel.count,
-                    isSelected = !basketModel.isChecked,
+                    isSelected = basketModel.isChecked,
                 )
             )
         }
@@ -178,16 +195,19 @@ class BasketListViewModel @Inject constructor(
 
     private suspend fun getHistoryItemList(): List<HistoryItem> {
         val historyList = mutableListOf<HistoryItem>()
-        basketItemFlow.first().forEach {
-            if (it.isChecked) {
-                historyList.add(
-                    HistoryItem(
-                        imageUrl = it.image,
-                        count = it.count,
-                        originPrice = it.price,
-                        name = it.name
+        val basketList = basketUiState.first()
+        if (basketList is UiState.Success) {
+            basketList.item.forEach {
+                if (it.isChecked) {
+                    historyList.add(
+                        HistoryItem(
+                            imageUrl = it.image,
+                            count = it.count,
+                            originPrice = it.price,
+                            name = it.name
+                        )
                     )
-                )
+                }
             }
         }
         return historyList
